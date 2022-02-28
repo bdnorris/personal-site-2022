69cef334643f40a9f5ff20219ecaac493dea5d72
Brian <bdnorris@gmail.com>
2022-02-27 19:39:47 -0600

    sanity body working with block content to react

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { graphql } from 'gatsby'
 import Layout from "./main.js";
+import BlockContent from '@sanity/block-content-to-react'
+
+
+const BlockRenderer = (props) => {
+	const { style = 'normal' } = props.node
+
+	if (/^h\d/.test(style)) {
+		const level = style.replace(/[^\d]/g, '')
+		return React.createElement(style, { className: `heading-${level}` }, props.children)
+	}
+
+	if (style === 'blockquote') {
+		return <blockquote>- {props.children}</blockquote>
+	}
+
+	// Fall back to default handling
+	return BlockContent.defaultSerializers.types.block(props)
+}
 
 export const query = graphql`
 query MyQuery($slug: String!) {
 	sanityPost(slug: {current: {eq: $slug}}) {
 		_createdAt(locale: "", fromNow: false, formatString: "", difference: "")
 		title
+		_rawBody
 		body {
 			children {
 				marks
@@ -25,14 +44,9 @@ const ProjectPage = (data) => {
 			<h1>{data.data.sanityPost.title}</h1>
 			<article>
 				{
-					(data.data.sanityPost.body) ?
-						data.data.sanityPost.body[0].children.map((child) => {
-							return (
-								<p key={child.text}>
-									{child.text}
-								</p>
-							)
-						}) : null
+					(data.data.sanityPost._rawBody) ?
+						<BlockContent blocks={data.data.sanityPost._rawBody} serializers={{ types: { block: BlockRenderer } }} />
+						: null
 				}
 			</article>
 		</Layout>
