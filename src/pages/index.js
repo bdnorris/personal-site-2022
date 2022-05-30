c723bfd125f7e0aac48d3af2c5e51319ec18d8ad
Brian <bdnorris@gmail.com>
2022-05-30 15:50:43 -0500

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,58 +9,61 @@ import Shape from "../components/Shape";
 // data
 export const query = graphql`
 query posts {
-  allSanityPost {
-    edges {
-      node {
-        title
-        slug {
-          current
-        }
-        _updatedAt
-        categories {
-          title
-          id
-        }
-      }
-    }
-  }
-  allSanityCategory {
-    edges {
-      node {
-        title
-        id
-      }
-    }
-  }
+	allSanityPost {
+		edges {
+			node {
+				title
+				slug {
+					current
+				}
+				_updatedAt
+				categories {
+					title
+					id
+				}
+			}
+		}
+	}
+	allSanityCategory {
+		edges {
+			node {
+				title
+				id
+			}
+		}
+	}
 }
 `;
 
 // markup
 const IndexPage = ({ data }) => {
-  console.log(data);
-  const categories = data.allSanityCategory.edges;
-  const posts = data.allSanityPost.edges;
-  return (
-    <Layout>
-      <title>Home Page</title>
-      <h1 style={{ margin: '1em 0 0 0' }}>
-        Brian N<Shape></Shape>rris
-      </h1>
-      <h2 style={{ margin: '0' }}>
-        Web Developer
-      </h2>
-      <nav>
-        <ul>
-          {categories.map(({ node }) => (
-            <li key={node.id}>
-              <h3>{node.title}</h3>
-              <CategoryListing category={node.id} posts={posts} />
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </Layout>
-  );
+	console.log(data);
+	const categories = data.allSanityCategory.edges;
+	const posts = data.allSanityPost.edges;
+	return (
+		<Layout>
+			<div className="main-wrapper">
+				<div>
+				<h1 style={{ margin: '1em 0 0 0' }}>
+					Brian N<Shape></Shape>rris
+				</h1>
+				<h2 style={{ margin: '0' }}>
+					Web Developer
+				</h2>
+				<nav>
+					<ul>
+						{categories.map(({ node }) => (
+							<li key={node.id}>
+								<h3>{node.title}</h3>
+								<CategoryListing category={node.id} posts={posts} />
+							</li>
+						))}
+					</ul>
+				</nav>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default IndexPage
